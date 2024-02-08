a1746a4727dd2e877d389fa7545e2a0b97876bad
chel12 <sema.chel12@gmail.com>
2024-02-08 16:44:19 +0300

    +styles from AuthPage + AuthForm

--- src/components/AuthForm/AuthForm.tsx
+++ src/components/AuthForm/AuthForm.tsx
@@ -1,8 +1,15 @@
+import Typography from '@mui/material/Typography';
 import React from 'react';
 import './AuthForm.css';
 
 const AuthForm: React.FC = () => {
-	return <div className="auth-form">AuthForm</div>;
+	return (
+		<div className="auth-form">
+			<Typography variant="h4" gutterBottom component="div">
+				h1. Heading
+			</Typography>
+		</div>
+	);
 };
 
 export default AuthForm;
